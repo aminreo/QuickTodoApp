39b4f3aad4a0df648a8d97c28df805e7e9f99aef
Amin <ayari.am@campus.lmu.de>
2026-09-13 17:38:37 +0200

    working prototype

--- app.py
+++ app.py
@@ -1,36 +1,53 @@
 import json
 from tkinter import *
 from tkinter import ttk
-
-root = Tk(className="Welcome to the Todo App!")
-
-main_frame = ttk.Frame(root,padding=10)
-main_frame.grid()
-
-ttk.Button(main_frame, text="add",command= lambda : addTodoWindow()).grid(column=0, row=0)
-
-frm = ttk.Frame(main_frame, padding=10)
-frm.grid()
-def saveNewTodo():
-     #todo
-     return
+import uuid
 
 def addTodoWindow():
      addTodoWindow_frame = Toplevel(padx=10,pady=10)
     #  addTodoWindow_frame.grid()
      ttk.Label(addTodoWindow_frame,text="add new habit").pack()
     #  .grid(column=0,row=0)
-    #  entry=
-     ttk.Entry(addTodoWindow_frame,textvariable="a").pack()
-     ttk.Button(addTodoWindow_frame,text="save",command= lambda: saveNewTodo()).pack()
+     entry= ttk.Entry(addTodoWindow_frame,textvariable=new_habit_var)
+    #  entry.bind("<Return>",lambda e : saveNewTodo(new_habit_var.get()))
+     entry.pack()
+     ttk.Button(addTodoWindow_frame,text="save",command= lambda: saveNewTodo(new_habit_var.get())).pack()
 
     #  entry.grid(column=0,row=1)
      return
+def save_to_json():
+     with open ("text.json","w") as file:
+             # file.write("[]")
+             json.dump((habits_list_tmp),fp=file)
+
+root = Tk(className="Welcome to the Todo App!")
+new_habit_var = StringVar(value="")
+main_frame = ttk.Frame(root,padding=10)
+main_frame.grid()
+
+ttk.Button(main_frame, text="add",command= addTodoWindow).grid(column=0, row=0)
+
+frm = ttk.Frame(main_frame, padding=10)
+frm.grid()
+def saveNewTodo(s):
+     #todo
+     if not s:
+          print("invalid input")
+          return
+     print(s)
+     new_id= str(uuid.uuid4())
+     habits_list_tmp.append({"id":new_id, "name":s,"done":False})
+     save_to_json()
+     render()
+     return
+
 
 def done(s):
-    for todo in testing_list:
+    # print(f"id {s}")
+    for todo in habits_list_tmp:
         if todo.get("id") == s:
             todo["done"]=True
+    save_to_json()
     render()
             
 def render():
@@ -38,25 +55,28 @@ def render():
     for item in frm.winfo_children():
         item.destroy()
 
-    for i,todo in enumerate(testing_list):
+    for i,todo in enumerate(habits_list_tmp):
                 if todo.get("done") == True:
                     continue
                 ttk.Label(frm, text=todo.get("name")).grid(column=0, row=i)
                 todo_id=todo.get("id")
-                ttk.Button(frm, text="done",command=lambda : done(todo_id)).grid(column=1, row=i)
+                ttk.Button(frm, text="completed",command=lambda id=todo_id : done(id)).grid(column=1, row=i)
                 empty_list=False
     if empty_list:
             ttk.Label(frm, text="empty").grid(column=0, row=1)
 
-    root.mainloop()
+    
+
+# habits_list_tmp = [{"id":str(uuid.uuid4()),"name":"1","done":False},{"id":str(uuid.uuid4()),"name":"2","done":False},{"id":str(uuid.uuid4()),"name":"3","done":True}]
 
 try:
-    with open ("text.json","r") as file:
-        json.load(file)
-except json.JSONDecodeError: 
-    with open ("text.json","w") as file:
-        file.write("[]")
+    with open("text.json","r") as file:
+        habits_list_tmp= json.load(file)
+except (json.JSONDecodeError,FileNotFoundError): 
+    habits_list_tmp=[]
+    save_to_json()
 
-testing_list = [{"id":111,"name":"1","done":False},{"id":112,"name":"2","done":False},{"id":113,"name":"3","done":True}]
 
 render()
+root.mainloop()
+# json.dump(habits_list_tmp,fp=file)
